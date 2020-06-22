27a4579f1014b2fc1b81a38a2e503c596c13f03a
Sanam Malik <sanam.malik04@gmail.com>
2020-06-22 21:11:33 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,12 @@
-def introduction name
+def introduction(name)
+  name = "Sanam"
 puts "Hi, my name is #{name}."
+end
 
+def introduction_with_language name and language
+  puts "Hi, my name is #{name} and I am leanring to program in #{language}."
+  
+  def introduction_with_language_optional name and language 
 
 
 
